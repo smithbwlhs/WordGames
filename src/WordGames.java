e48645b1afb25791ed23babe472295f05741b98a
smithb <smithb@wlwv.k12.or.us>
2022-10-17 08:16:39 -0700

    Initial commit

---
+++ src/WordGames.java
@@ -0,0 +1,41 @@
+public class WordGames
+{
+    private String word;
+
+    public WordGames(String text)
+    {
+        word = text;
+    }
+
+    public String scramble()
+    {
+        // switch first half
+        // and second half
+
+    }
+
+
+    public String bananaSplit(int insertIdx, String insertText)
+    {
+        // Insert insertText at the position
+        // insertIdx
+
+    }
+
+
+    public String bananaSplit(String insertChar, String insertText)
+    {
+        // Insert insertText after the first
+        // occurence of the insertChar
+
+    }
+
+
+    public String toString()
+    {
+        // Games[word]
+
+    }
+
+
+}
